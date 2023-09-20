735abdefeceba652350ad740badd2ca9afb17b49
Kit Lam <AkaKaras@users.noreply.github.com>
2023-09-20 14:36:15 -0400

    :wrench: Update the map view config

--- src/components/arcgis/MapViewDiv.tsx
+++ src/components/arcgis/MapViewDiv.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import ArcGISMap from '@arcgis/core/Map.js';
 import MapView from '@arcgis/core/views/MapView.js';
 import { BaseView } from '../configs/mapViewConfig';
+
 export const MapViewDiv = () => {
 	useEffect(() => {
 		const map = new ArcGISMap({
@@ -9,10 +10,10 @@ export const MapViewDiv = () => {
 		});
 		const view = new BaseView(map, 'viewDiv').init();
 
-		console.log(process.env.REACT_APP_MAP_DEFAULT_ZOOM);
 		view.when(() => {
 			console.log('load');
 		});
 	});
+
 	return <div id="viewDiv"></div>;
 };
