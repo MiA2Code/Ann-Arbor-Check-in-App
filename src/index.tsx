346e65ba5d6224d3a29156a6c80129845bba0a39
Kit Lam <AkaKaras@users.noreply.github.com>
2023-09-21 15:35:55 -0400

    :construction: Upload work to remote
    1. add python crawling script

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './css/index.scss';
 import App from './App';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import '@esri/calcite-components/dist/calcite/calcite.css';
+import { setAssetPath } from '@esri/calcite-components/dist/components';
+// CDN hosted assets
+setAssetPath('https://js.arcgis.com/calcite-components/1.8.0/assets');
 const root = createRoot(document.getElementById('root') as HTMLElement);
+
+const queryClient = new QueryClient();
 root.render(
-	<React.StrictMode>
+	// <React.StrictMode>
+	<QueryClientProvider client={queryClient}>
 		<App />
-	</React.StrictMode>
+		<ReactQueryDevtools />
+	</QueryClientProvider>
+	// </React.StrictMode>
 );
